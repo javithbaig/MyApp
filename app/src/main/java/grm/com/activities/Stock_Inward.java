5153f9036ef12f582d722e354721c2b8ca28437e
javithbaig <javithbaig18@gmail.com>
2019-06-13 13:06:19 +0530

    New Activities Added

---
+++ app/src/main/java/grm/com/activities/Stock_Inward.java
@@ -0,0 +1,15 @@
+package grm.com.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import grm.com.R;
+
+public class Stock_Inward extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock__inward);
+    }
+}
---
+++ app/src/main/java/grm/com/activities/Stock_Outward.java
@@ -0,0 +1,15 @@
+package grm.com.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import grm.com.R;
+
+public class Stock_Outward extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock__outward);
+    }
+}
