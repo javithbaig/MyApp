895702c208efc5ee4e2ad1cbee04fe873c25031e
javithbaig <javithbaig18@gmail.com>
2019-06-15 11:52:02 +0530

    New Activities Added

--- app/src/main/java/grm/com/activities/Home.java
+++ app/src/main/java/grm/com/activities/Home.java
@@ -1,15 +1,33 @@
 package grm.com.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.Button;
 
 import grm.com.R;
 
 public class Home extends AppCompatActivity {
 
+    private AppCompatButton btn_inward, btn_outward;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btn_inward = (AppCompatButton)findViewById(R.id.btn_inward);
+        btn_outward = (AppCompatButton)findViewById(R.id.btn_outward);
+
+        btn_inward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Stock_Inward.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/grm/com/activities/Stock_Inward.java
+++ app/src/main/java/grm/com/activities/Stock_Inward.java
@@ -1,15 +1,29 @@
 package grm.com.activities;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import grm.com.R;
 
 public class Stock_Inward extends AppCompatActivity {
-
+    String[] fruits = {"Apple", "Banana", "Cherry", "Date", "Grape", "Kiwi", "Mango", "Pear"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock__inward);
+
+        //Creating the instance of ArrayAdapter containing list of fruit names
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this, android.R.layout.select_dialog_item, fruits);
+        //Getting the instance of AutoCompleteTextView
+        AutoCompleteTextView actv = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        actv.setThreshold(1);//will start working from first character
+        actv.setAdapter(adapter);//setting the adapter data into the AutoCompleteTextView
+        actv.setTextColor(Color.RED);
     }
+
+
 }
